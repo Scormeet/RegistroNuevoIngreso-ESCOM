d8f362f93e5dccfb2c33b1680878f9bcff20a513
Scormit <sbenitezr@outlook.com>
2020-03-23 17:45:54 -0600

    Pequeño auste en mysqli_connect

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -2,7 +2,7 @@
   // Variables de conexion BD
   $servername = "localhost";
   $username = "root";
-  $password = "yeyo1253";//"N0M3L0";
+  $password = "N0M3L0";
   $dbname  = "ingreso";
 
   // Create connection
